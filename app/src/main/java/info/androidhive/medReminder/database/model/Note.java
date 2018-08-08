271dc974a9640b8146ab73b06f79938fd967cedc
ajaykhanna123 <ajaykhanna123ak@gmail.com>
2018-08-08 20:41:14 +0530

    bnjc

---
+++ app/src/main/java/info/androidhive/medReminder/database/model/Note.java
@@ -0,0 +1,72 @@
+package info.androidhive.medReminder.database.model;
+
+/**
+ * Created by ravi on 20/02/18.
+ */
+
+public class Note {
+    public static final String TABLE_NAME = "notes";
+
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NOTE = "note";
+    public static final String COLUMN_TIME="time";
+    public static final String COLUMN_TIMESTAMP = "timestamp";
+
+
+    private int id;
+    private String note;
+    private String timestamp;
+    private String time;
+
+
+    // Create table SQL query
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_NOTE + " TEXT,"
+                    + COLUMN_TIME + " TEXT,"
+                    + COLUMN_TIMESTAMP + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+                    + ")";
+
+    public Note() {
+    }
+
+    public Note(int id, String note,String time, String timestamp) {
+        this.id = id;
+        this.note = note;
+        this.timestamp = timestamp;
+        this.time=time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
